bd2d372e775134a2e2b69db64bf86a1838508bf6
yaselramos <yaselramos@gmail.com>
2025-09-12 15:02:08 -0300

    proyecto factura

---
+++ src/test/java/com/factura/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factura.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
